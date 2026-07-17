74c601415e389d0b43f0853c6f07475e875781c6
namtxs <namdevel@gmail.com>
2026-07-17 20:35:56 +0700

    remove output files

--- .vercel/output/static/_app/immutable/entry/start.C0bvHjbj.js
+++
@@ -1,1 +0,0 @@
-import{l as o,d as r}from"../chunks/S6bxejIu.js";export{o as load_css,r as start};
--- scripts/test-embed.js
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs';
-import { sourceFromHtml } from '../src/lib/server/embedresolve.js';
-
-const html = fs.readFileSync('tmp-live.html', 'utf8');
-console.log('sourceFromHtml', sourceFromHtml(html, 'fox-usa'));
